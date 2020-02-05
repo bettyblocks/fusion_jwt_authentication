b92a44dbc626841ddfa996d2a438501f88e5c87e
Peter Arentsen <peter.arentsen@holder.nl>
2020-02-05 14:55:51 +0100

    Fix version to be sem version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FusionJWTAuthentication.MixProject do
   def project do
     [
       app: :fusion_jwt_authentication,
-      version: "0.2",
+      version: "0.2.0",
       elixir: "~> 1.9",
       description: description(),
       elixirc_paths: elixirc_paths(Mix.env()),
