f92f78a54ba87ae3c55c3009a679066197a866f5
Peter Arentsen <peter.arentsen@holder.nl>
2020-02-05 14:38:02 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FusionJWTAuthentication.MixProject do
   def project do
     [
       app: :fusion_jwt_authentication,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       description: description(),
       elixirc_paths: elixirc_paths(Mix.env()),
