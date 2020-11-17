6a20b600e93361417ed9792bb46411f669aa4c95
Peter Arentsen <peter.arentsen@holder.nl>
2020-11-17 10:28:07 +0100

    chore: also make it possible to return the not found error

--- lib/fusion_jwt_authentication/handle_login.ex
+++ lib/fusion_jwt_authentication/handle_login.ex
@@ -2,5 +2,5 @@ defmodule FusionJWTAuthentication.HandleLogin do
   @moduledoc """
   Behaviour for handling login
   """
-  @callback handle_login(Plug.Conn.t(), map) :: {:ok, Plug.Conn.t()} | {:error, Plug.Conn.t()}
+  @callback handle_login(Plug.Conn.t(), map) :: {:ok, Plug.Conn.t()} | {:error, Plug.Conn.t()} | :error
 end
